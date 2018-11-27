7e9896a9ab88f45b7dffbd6fe3a88e630e9ebf71
Alex Markowitz <admarko@gmail.com>
2018-11-27 01:10:38 -0500

    Started adding AI methods

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,13 @@
-# Simple Tic Tac Toe Game
+'''
+Simple Tic Tac Toe Game that lets users play in Terminal vs. an AI
+By Alex Markowitz
+github.com/admarko
+Last Modified November 2018
+'''
+
+#############################
+#### Board Class ####
+#############################
 
 class Board:
 	def __init__(self):
@@ -48,20 +57,61 @@ class Board:
 			print "\nGame over - " + winner + " wins!!!"
 			self.print_board()
 			exit()
-		
+
+#############################
+#### AI Methods #############
+#############################
+
+def two_move_one_dash(three, move):
+	return three.count(move) == 2 and three.count("-") == 1
+
+def ai_win_next_diag(board, move):
+	diag1 = [board.game_board[0][0], board.game_board[1][1], board.game_board[2][2]]
+	diag2 = [board.game_board[0][2], board.game_board[1][1], board.game_board[2][0]]
+	if two_move_one_dash(diag1, move) or two_move_one_dash(diag2, move):
+		print move + " can win next turn (Diag)"
+		return True
+	else: 
+		return False
+
+def ai_win_next_col(board, move):
+	col1 = [board.game_board[0][0], board.game_board[1][0], board.game_board[2][0]]
+	col2 = [board.game_board[0][1], board.game_board[1][1], board.game_board[2][1]]
+	col3 = [board.game_board[0][2], board.game_board[1][2], board.game_board[2][2]]
+	if two_move_one_dash(col1, move) or two_move_one_dash(col2, move) or two_move_one_dash(col3, move):
+		print move + " can win next turn (COL)"
+		return True
+	else: 
+		return False
+
+def ai_win_next_row(board, move):
+	# check for row winner
+	if two_move_one_dash(board.game_board[0], move) or two_move_one_dash(board.game_board[1], move) or two_move_one_dash(board.game_board[2], move):
+		print move + " can win next turn (row)"
+		return True
+	else: 
+		return False
 			
 def simple_ai(board):
 	print "\nAI's move:"
+
+	# if ai_win_next_row(board, "O") or ai_win_next_col(board, "O") or ai_win_next_diag(board, "O"):
+	# 	print "Can win next move!"
+
 	for i in range(3):
 		for j in range(3):
 			if board.game_board[i][j] == "-":
 				board.add_move("O", i, j)
 				return
 
+#############################
+#### User + Main Methods ####
+#############################
+
 # Method to 	
 def user_move(board):
 	while True:
-		print "Your turn, where would you like to move?"
+		print "It's your turn, where would you like to move?"
 		try:
 			row = int(raw_input("row: "))
 			col = int(raw_input("col: "))
